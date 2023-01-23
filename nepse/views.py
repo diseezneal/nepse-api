225a2953f3742edb3131a352a422b4a4feffb8b9
Aakash Basnet <iamaakashbasnet@gmail.com>
2023-01-23 20:43:54 +0545

    feat: todays share price endpoint

--- nepse/views.py
+++ nepse/views.py
@@ -1,7 +1,17 @@
+import csv
+from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
 class TodaysPrice(APIView):
     def get(self, request, format=None):
-        return Response({})
+        res = []
+
+        with open(settings.BASE_DIR / 'data/todaysprice.csv', 'r', encoding='UTF-8') as csvf:
+            csvReader = csv.DictReader(csvf)
+
+            for row in csvReader:
+                res.append(row)
+
+        return Response(res)
